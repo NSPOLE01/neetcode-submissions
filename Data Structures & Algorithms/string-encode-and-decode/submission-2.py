ac8fa174a81549a51e223de866f71ead9c667a53
Nikhil Polepalli <89700736+NSPOLE01@users.noreply.github.com>
2026-08-09 13:50:05 -0400

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        result = ""
+        for s in strs:
+            result = result + s + "<"
+        return result
+    
+    def decode(self, s: str) -> List[str]:
+        substring = ""
+        result = []
+        for c in s:
+            if c == "<":
+                result.append(substring)
+                substring = ""
+            else:
+                substring = substring + c
+
+        return result
+
