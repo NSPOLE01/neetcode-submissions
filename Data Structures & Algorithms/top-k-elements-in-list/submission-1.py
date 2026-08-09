30113d547fd8247df7451cbfce66e5ab5174cddc
Nikhil Polepalli <89700736+NSPOLE01@users.noreply.github.com>
2026-08-09 13:38:30 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        num_map = Counter(nums)
+        result = []
+        num_list = list(num_map)
+        num_list.sort(reverse=True)
+        print(num_list)
+
+        for i in range(k):
+            result.append(num_list[i])
+            i += 1
+        return result
+
+
+        
